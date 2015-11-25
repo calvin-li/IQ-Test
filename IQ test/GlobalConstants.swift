20d579c6314b557a2a48d443411d077321758270
Calvin Li <calli@ucdavis.edu>
2015-11-24 16:14:13 -0800

    changed to using collectionViewController

---
+++ IQ test/GlobalConstants.swift
@@ -0,0 +1,14 @@
+//
+//  GlobalConstants.swift
+//  IQ test
+//
+//  Created by Calvin Li on 11/23/15.
+//  Copyright © 2015 UCD SE Lab. All rights reserved.
+//
+
+import Foundation
+
+struct GlobalConstants {
+    static let patternSize = 9
+    static let numberOfChoices = 8
+}
