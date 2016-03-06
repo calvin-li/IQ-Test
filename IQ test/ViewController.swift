2239240a334d314652e261ea3a36fb4522f37a4a
Calvin Li <calli@ucdavis.edu>
2016-03-05 23:30:51 -0800

    finished\!

--- IQ test/ViewController.swift
+++ IQ test/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 import SVGKit
 
-let answers = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+//let answers = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+let answers = [2,1,0,2,0,3,4,3,0,6,4,2,7,4,5,2,4,0,5,1] //real answers
 
 class ViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
 
@@ -20,7 +21,6 @@ class ViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICo
     
     private var sampleProblems: [(question: [String], choices: [String])] = []
     private var pIndex = 0
-    //private let answers = [2,1,0,2,0,3,4,3,0,6,4,2,7,4,5,2,4,0,5,1] //real answers
     
     private var tries: UILabel!
     private let totalTries = 3
@@ -30,6 +30,8 @@ class ViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICo
     private var timer: NSTimer!
     private let totalTime = 6 * answers.count
     private var time: Int!
+    
+    private var revealed = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -93,18 +95,19 @@ class ViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICo
         let scoreH = choiceLayout.sectionInset.top - scoreY - 10
         
         scoreView = UIView(frame: CGRect(x: scoreX, y: scoreY, width: scoreW, height: scoreH))
-        scoreView.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.1)
         self.view.addSubview(scoreView)
         
-        let scoreItemSpacing: CGFloat = 10
+        let scoreItemSpacing: CGFloat = 5
+        let scoreBGColor = UIColor.whiteColor().colorWithAlphaComponent(0.5)
         let scoreFont = UIFont.boldSystemFontOfSize(scoreH/3)
         
-        let scoreTextW = scoreW/3
+        let scoreTextW = scoreW/2
         
         var newX: CGFloat = 0.0
         
         scoreText = UILabel(frame: CGRect(x: newX, y: 0.0, width: scoreTextW, height: scoreH/2))
-        scoreText.backgroundColor = UIColor.whiteColor()
+        scoreText.bounds = CGRectInset(scoreText.frame, scoreItemSpacing, scoreItemSpacing)
+        scoreText.backgroundColor = scoreBGColor
         scoreText.textAlignment = NSTextAlignment.Center
         numRight = 0; numDone = 0
         updateScore()
@@ -112,34 +115,68 @@ class ViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICo
         scoreView.addSubview(scoreText)
         
         
-        let timerTextW = scoreTextW
+        let timerTextW = scoreTextW/2
+        
         timerText = UILabel(frame: CGRect(x: newX, y: scoreH/2, width: timerTextW, height: scoreH/2))
-        timerText.backgroundColor = UIColor.whiteColor()
+        timerText.bounds = CGRectInset(timerText.frame, scoreItemSpacing, scoreItemSpacing)
+        timerText.backgroundColor = scoreBGColor
         timerText.textAlignment = NSTextAlignment.Center
         timerText.font = scoreFont
         time = totalTime
         timerText.text = String(time)
         scoreView.addSubview(timerText)
         
-        newX += scoreText.frame.width
+        newX += timerText.frame.width + scoreItemSpacing*2
         
-        let triesW = scoreW/9
-        tries = UILabel(frame: CGRect(x: newX, y: 0.0, width: triesW, height: scoreH))
-        tries.backgroundColor = UIColor.yellowColor()
+        let triesW = scoreTextW/2
+        tries = UILabel(frame: CGRect(x: newX, y: scoreH/2, width: triesW, height: scoreH/2))
+        tries.bounds = CGRectInset(tries.frame, scoreItemSpacing, scoreItemSpacing)
+        tries.backgroundColor = scoreBGColor
         tries.textAlignment = NSTextAlignment.Center
         tries.font = scoreFont
         updateTries(totalTries)
         scoreView.addSubview(tries)
         
-        newX += tries.frame.width
+        newX += tries.frame.width + scoreItemSpacing
         
-        let restartButtonW = scoreW * 5/27
+        let restartButtonW = scoreW / 6
         let restartButton = UIButton(type: .System)
         restartButton.frame = CGRect(x: newX, y: 0.0, width: restartButtonW, height: scoreH)
-        restartButton.backgroundColor = UIColor.darkGrayColor()
+
+        restartButton.setTitle("↻", forState: .Normal)
+        restartButton.titleLabel!.font = UIFont.boldSystemFontOfSize(scoreH)
+        
+        restartButton.addTarget(self, action: "restart", forControlEvents: .TouchUpInside)
+        restartButton.setTitleColor(UIColor.redColor(), forState: .Normal)
         scoreView.addSubview(restartButton)
         
-        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "tick", userInfo: nil, repeats: true)
+        newX += restartButton.frame.width
+        
+        let revealButtonW = restartButtonW
+        let revealButton = UIButton(type: .System)
+        revealButton.frame = CGRect(x: newX, y: 0.0, width: revealButtonW, height: scoreH)
+        
+        revealButton.setTitle("🔍", forState: .Normal)
+        revealButton.titleLabel!.font = UIFont.boldSystemFontOfSize(scoreH * 0.7)
+        revealButton.setTitleColor(UIColor.greenColor(), forState: .Normal)
+        
+        revealButton.addTarget(self, action: "revealAnswer", forControlEvents: .TouchUpInside)
+        scoreView.addSubview(revealButton)
+        
+        newX += revealButton.frame.width
+        
+        let testButtonW = restartButtonW
+        let testButton = UIButton(type: .System)
+        testButton.frame = CGRect(x: newX, y: 0.0, width: testButtonW, height: scoreH)
+        
+        testButton.setTitle("🕐", forState: .Normal)
+        testButton.titleLabel!.font = UIFont.boldSystemFontOfSize(scoreH * 0.7)
+        testButton.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        
+        testButton.addTarget(self, action: "startPractice", forControlEvents: .TouchUpInside)
+        scoreView.addSubview(testButton)
+        
+        startTimer()
     }
 
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
@@ -190,16 +227,19 @@ class ViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICo
     
     func pressed(sender: UIButton!){
         unhighlight(sender)
-        if (time == 0) { return }
-        else if (Int(sender.accessibilityIdentifier!) == answers[pIndex]){
+        if (time == 0) {
+            return
+        } else if (pIndex >= answers.count){ //game over
+            return
+        } else if (Int(sender.accessibilityIdentifier!) == answers[pIndex]){
+            if (!revealed) {numRight++}
+            //move on to next problem
             pIndex++
-            questionView.reloadData()
-            choicesView.reloadData()
             numDone++
-            if (numTries > 0) {numRight++}
-            updateTries(totalTries)
+            reset()
+            
         } else {
-            updateTries(max(0, numTries-1))
+            updateTries(numTries-1)
             if (numTries == 0) {
                 revealAnswer()
             }
@@ -207,9 +247,42 @@ class ViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICo
         updateScore()
     }
     
+    func startTimer(){
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "tick", userInfo: nil, repeats: true)
+    }
+    
+    func restart(){
+        pIndex = 0
+        numDone = 0
+        numRight = 0
+        
+        reset()
+        
+        timerText.alpha = 1
+        time = totalTime
+        startTimer()
+        
+    }
+    
+    func reset(){
+        updateTries(totalTries)
+        
+        if(pIndex < answers.count){
+            revealed = false
+            questionView.reloadData()
+            choicesView.reloadData()
+        }
+    }
+    
+    func startPractice(){
+        timer.invalidate()
+        timerText.alpha = 0.5
+    }
+    
     func revealAnswer(){
             answerButton.layer.borderColor = UIColor.greenColor().colorWithAlphaComponent(0.5).CGColor
             answerButton.layer.borderWidth = 3
+            revealed = true
     }
     
     internal func tick(){
@@ -223,7 +296,7 @@ class ViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICo
     
     func updateTries(n: Int){
         numTries = n
-        if(numTries == 0){
+        if(numTries <= 0){
             tries.textColor = UIColor.redColor()
         } else {
             tries.textColor = UIColor.blackColor()
